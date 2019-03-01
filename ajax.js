bea14a5d5702f38d57e972c24692c3f59b911dbc
Sai Sri Dachepalli <s534883@nwmissouri.edu>
2019-03-01 13:11:48 -0600

    adding changes

--- ajax.js
+++ ajax.js
@@ -1,10 +1,11 @@
+var regex=/^[0-9]+$/;
 $(document).ready(function(){
     $("button").click(function(){
         var a = $('#num1').val();
         var b = $('#num2').val();
         var c = $('#num3').val();
         
-let mul = multiply(a,b);
+let mul = multiply(a,b,c);
 alert("Average is :"+mul);
 
     });
@@ -14,3 +15,18 @@ alert("Average is :"+mul);
 function multiply(x,y,z) {
     return (parseInt(x)+parseInt(y)+parseInt(z))/3;
 }
+function mouseHOver() {
+    const xhttp = new XMLHttpRequest();
+    $.ajax({
+      type: 'GET',
+      url: 'https://thesimpsonsquoteapi.glitch.me/quotes?count=4',
+      success: function (data) {
+        $("#mouseOverExamp").html(data[1].quote);
+      }
+    });
+  }
+  
+  function mouseOut() {
+    $("#mouseOverExamp").html("Hey, place the cursor on me");
+  }
+  
